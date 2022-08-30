cc3bfb19bbe748d63cc167de2f2b00ccf21b10aa
shirleyqi26 <69826368+shirleyqi26@users.noreply.github.com>
2022-08-30 14:24:39 -0700

    finished mocked login page

--- js/loginHandler.js
+++ js/loginHandler.js
@@ -1,3 +1,9 @@
 function handleLogin(){
-    
+    var form = $('form').serializeArray();
+
+    if((form[0].value == "ruffles") && (form[1].value == "ruffles123")){
+        window.location = "../html/landing.html";
+    }else{
+        $("#incorrect_credentials").show();
+    }
 }
